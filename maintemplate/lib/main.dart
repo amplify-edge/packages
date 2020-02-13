a566c8b21912175d8d4e57bd9cab6ca463389db7
winwisely111 <60743306+winwisely111@users.noreply.github.com>
2020-02-13 15:17:33 +0500

    Added responsive scaffold and master detail scaffold

--- maintemplate/lib/main.dart
+++ maintemplate/lib/main.dart
@@ -1,49 +1,68 @@
 import 'package:flutter/material.dart';
-import 'package:maintemplate/features/responvive_builder_example/responsive_template.dart';
+import 'package:maintemplate/features/master_detail_scaffold_example/data/mock_item.dart';
+import 'package:maintemplate/features/master_detail_scaffold_example/master_detail_view.dart';
+import 'package:maintemplate/features/responsive_scaffold_example/responsive_scaffold_view.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(MaterialApp(
-  debugShowCheckedModeBanner: false,
-  home: App()));
+import 'features/responvive_builder_example/responsive_template.dart';
+
+void main() =>
+    runApp(Provider<List<MockItem>>(
+      create : (context) => mockItemsList,
+      child: MaterialApp(debugShowCheckedModeBanner: false, home: App())));
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-            body: Flex(
-          direction: Axis.vertical,
-          children: <Widget>[
-            Flexible(
-              flex: 1,
-              child: InkWell(
-                onTap: () {
-                  Navigator.push(context,  MaterialPageRoute(builder: (context){
-                      return ResponsiveTemplate();
-                    }));
-                
-                  
-                },
-                child: Container(
-                    child: Center(
-                  child: Text("Responsive Builder"),
-                )),
-              ),
-            ),
-            Divider(),
-            Flexible(
-              flex: 1,
-              child: InkWell(
-                onTap: () {
-                    Navigator.push(context,  MaterialPageRoute(builder: (context){
-                      return ResponsiveTemplate();
-                    }));
-                },
-                child: Container(
-                    child: Center(
-                  child: Text("Responsive Scaffold"),
-                )),
-              ),
-            ),
-          ],
-        ));
+    return Scaffold(
+        body: Flex(
+      direction: Axis.vertical,
+      children: <Widget>[
+        Flexible(
+          flex: 1,
+          child: InkWell(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return ResponsiveTemplate();
+              }));
+            },
+            child: Container(
+                child: Center(
+              child: Text("Responsive Builder"),
+            )),
+          ),
+        ),
+        Divider(),
+        Flexible(
+          flex: 1,
+          child: InkWell(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return ResponsiveScaffoldView();
+              }));
+            },
+            child: Container(
+                child: Center(
+              child: Text("Responsive Scaffold"),
+            )),
+          ),
+        ),
+        Divider(),
+        Flexible(
+          flex: 1,
+          child: InkWell(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return MasterDetailView();
+              }));
+            },
+            child: Container(
+                child: Center(
+              child: Text("Master Detail Scaffold"),
+            )),
+          ),
+        ),
+      ],
+    ));
   }
 }
