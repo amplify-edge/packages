a66c1e18d5dac1dfd63279725e2e5fbc15269c08
winwisely111 <winwisely111@gmail.com>
2020-03-09 22:19:53 +0500

    updated make file

--- maintemplate/client/lib/generated_plugin_registrant.dart
+++ maintemplate/client/lib/generated_plugin_registrant.dart
@@ -5,14 +5,12 @@
 // ignore: unused_import
 import 'dart:ui';
 
-import 'package:image_picker_web/image_picker_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins(PluginRegistry registry) {
-  ImagePickerWeb.registerWith(registry.registrarFor(ImagePickerWeb));
   SharedPreferencesPlugin.registerWith(registry.registrarFor(SharedPreferencesPlugin));
   UrlLauncherPlugin.registerWith(registry.registrarFor(UrlLauncherPlugin));
   registry.registerMessageHandler();
