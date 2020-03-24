5b1af36c4d0f8db2bb1b998315e2f424b286de16
winwisely111 <winwisely111@gmail.com>
2020-03-24 17:29:14 +0500

    data table added

--- mod-main/client/lib/modules/org_manager/orgs/views/orgs_dashboard_view.dart
+++ mod-main/client/lib/modules/org_manager/orgs/views/orgs_dashboard_view.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:mod_main/modules/org_manager/orgs/view_model/orgs_view_model.dart';
 import 'package:mod_main/modules/org_manager/orgs/views/orgs_dashboard_view_mobile.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 
 import 'orgs_dashboard_view_desktop.dart';
 
 class OrgsDashboardView extends StatelessWidget {
+  final OrgsViewModel model;
+
+  const OrgsDashboardView({Key key, this.model}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ResponsiveBuilder(
         builder: (context, sizingInfo) {
           if (sizingInfo.screenSize.width >= 1100.0) {
-            return OrgDashboardDesktopView();
+            return OrgDashboardDesktopView(model : model);
           }
 
-          return OrgDashboardMobileView();
+          return OrgDashboardMobileView(model : model);
         },
       ),
     );
