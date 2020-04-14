83b8ed02a1ad7b2d2202577a9227ada33a138b3a
winwisely111 <winwisely111@gmail.com>
2020-04-14 21:36:54 +0500

    data table fixed

--- mod-main/client/lib/modules/main_module.dart
+++ mod-main/client/lib/modules/main_module.dart
@@ -1,8 +1,10 @@
 
 
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:meta/meta.dart';
 import 'package:mod_main/core/core.dart';
 
+import 'org_manager/orgs/views/orgs_manager_view_mobile.dart';
 import 'orgs/service/orgs_service.dart';
 import 'orgs/views/org_view.dart';
 import 'splash/views/splash_view.dart';
@@ -15,25 +17,25 @@ import '../modules/org_manager/orgs/views/orgs_manager_view.dart';
 
 class MainAppModule extends ChildModule{
 
-  static String baseRoute;
+  final String baseRoute;
+  final String url;
+  final String urlNative;
 
-  static String cutOffBaseRoute(String route) {
-    if (route.indexOf(baseRoute) < 0) return route;
-    return route.substring(
-        route.indexOf(baseRoute) + baseRoute.length, route.length);
-  }
+  // static String cutOffBaseRoute(String route) {
+  //   if (route.indexOf(baseRoute) < 0) return route;
+  //   return route.substring(
+  //       route.indexOf(baseRoute) + baseRoute.length, route.length);
+  // }
 
-  MainAppModule(String baseRoute) {
-    if(baseRoute == '/'){
-      baseRoute = '';
-    }
-    assert(baseRoute != null);
-    MainAppModule.baseRoute = baseRoute;
-  }
+  MainAppModule({String baseRoute,  String url, String urlNative,}) : 
+   this.baseRoute = (baseRoute == '/') ? '' : baseRoute,
+   this.url  = url,
+   this.urlNative = urlNative;
 
   @override
   List<Bind> get binds => [
       Bind((i) => Paths(baseRoute)),
+      Bind((i) => EnvConfig(url , urlNative)),
       Bind((i) => OrgsService()),
       Bind((i) => SupportRoleService())
   ];
@@ -45,7 +47,8 @@ class MainAppModule extends ChildModule{
     Router("/userInfo", child: (_, args) => UserInfoView()),
     Router("/orgs", child: (_, args) => OrgView()),
     Router("/myneeds/orgs/:id", child: (_, args) => UserNeedsView(orgID: args.params['id'],)),
-    Router("/supportRoles/orgs/:id", child: (_, args) => SupportRoleView(orgId: args.params['id'],))
+    Router("/supportRoles/orgs/:id", child: (_, args) => SupportRoleView(orgId: args.params['id'],)),
+     Router("/orgs/:id", child: (_, args) => OrgsDetailViewMobile(orgID: args.params['id'], model : args.data,)),
   ];
 
   static Inject get to => Inject<MainAppModule>.of();
