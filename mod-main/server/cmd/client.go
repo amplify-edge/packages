4a2f6171e72cd0c5500924b06fc5c7446b7db83b
idir <Winwisly100>
2020-04-21 04:20:08 -0700

    added golang client allows to upload files to server

--- mod-main/server/cmd/client.go
+++ mod-main/server/cmd/client.go
@@ -3,8 +3,12 @@ package main
 import (
 	"context"
 	"crypto/tls"
+	"encoding/base64"
+	"errors"
 	"flag"
 	"fmt"
+	"io"
+	"math"
 	"os"
 	"path/filepath"
 	"time"
@@ -21,6 +25,8 @@ import (
 
 // TODO extend for env vars needed
 // Env vars should be in the k8s deployment (i.e. helm charts via secrets)
+
+const fileChunk = 1 * (1 << 12) // 1 MB, change this to your requirement
 
 func main() {
 	// printTestDataFiles()
@@ -55,10 +61,16 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 8*time.Second)
 	defer cancel()
 
+	// upload a file to server
+	err = newFile(ctx, c, "data/outputs/datadump/json/campaign/1at5cSl3cvQM59r4f_acwZ7Tl6HUwK4So.png", true)
+	if err != nil {
+		log.Fatalln("Got an error when upload the file", err)
+	}
+
 	// Migrate csv's to minio
 	_, err = c.Migrate(ctx, &pb.MigrateRequest{Datapath: "data/outputs/datadump/"})
 	if err != nil {
-		log.Fatalf("Error while creating new answer: %v", err)
+		log.Fatalf("Error while Migrate data: %v", err)
 	}
 
 	campaings, err := c.ListCampaigns(ctx, &pb.ListCampaignRequest{})
@@ -127,4 +139,100 @@ func printTestDataFiles() {
 	for _, file := range files {
 		fmt.Println(file)
 	}
+}
+
+func newFile(ctx context.Context, c pb.QuestionClient, filePath string, useAck bool) error {
+	file, err := os.Open(filePath)
+
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	fileInfo, err := file.Stat()
+	if err != nil {
+		return err
+	}
+
+	var fileSize int64 = fileInfo.Size()
+	totalChunks := uint64(math.Ceil(float64(fileSize) / float64(fileChunk)))
+
+	i := uint64(1)
+	partSize := int64(math.Min(fileChunk, float64(fileSize-int64(i*fileChunk))))
+	if fileSize < fileChunk {
+		partSize = fileSize
+	}
+
+	buffer := make([]byte, partSize)
+
+	// first chunk used to send info about file like name, extension and total of chunks.
+	firstChunk := &pb.Chunk{
+		File: &pb.File{
+			Name:        fileInfo.Name(),
+			TotalChunks: totalChunks,
+		},
+	}
+
+	// Subscribe to stream
+	stream, err := c.NewFile(ctx)
+	if err != nil {
+		return err
+	}
+
+	// send first chunk to server
+	err = stream.Send(firstChunk)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if useAck {
+		// wait ti recive the ack from server
+		ack, err := stream.Recv()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// check if there is an ok from the server to start sending chunks
+		if !ack.Status {
+			return errors.New("Error Ack status")
+		}
+	}
+
+	fmt.Println("------------------------------------------------")
+	fmt.Println(firstChunk.File)
+	fmt.Println("------------------------------------------------")
+	for {
+		file.Read(buffer)
+		// encode chunk to base64
+		dataEnc := base64.StdEncoding.EncodeToString(buffer)
+
+		chunk := &pb.Chunk{
+			Id:   i,
+			Data: dataEnc,
+		}
+
+		err = stream.Send(chunk)
+		fmt.Println("chunk sended: ", i)
+		if err != nil && err != io.EOF {
+			return err
+		}
+
+		// wait to recive the ack from server
+		ack, err := stream.Recv()
+		if err != nil && err != io.EOF {
+			return err
+		}
+		fmt.Println("Ack status ", ack.Status)
+		// check if there is an ok from the server to start sending chunks
+		if !ack.Status {
+			return errors.New("Error Ack status")
+		}
+
+		if i == totalChunks {
+			break
+		}
+
+		i++
+	}
+	return nil
 }
--- mod-main/server/go.mod
+++ mod-main/server/go.mod
@@ -4,7 +4,7 @@ go 1.14
 
 require (
 	github.com/getcouragenow/bootstrap v0.0.0-20200414223544-1bb4cfa06050 // indirect
-	github.com/golang/protobuf v1.3.5
+	github.com/golang/protobuf v1.4.0-rc.4.0.20200313231945-b860323f09d0
 	github.com/johnsiilver/getcert v0.0.0-20190816170103-14357049f896
 	github.com/joho/godotenv v1.3.0
 	github.com/mattn/go-colorable v0.1.6 // indirect
@@ -12,4 +12,5 @@ require (
 	github.com/rotisserie/eris v0.3.0 // indirect
 	golang.org/x/sys v0.0.0-20200413165638-669c56c373c4 // indirect
 	google.golang.org/grpc v1.28.1
+	google.golang.org/protobuf v1.21.0
 )
