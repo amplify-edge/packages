aa70685ca1c84cb64248a7d528f8b9016c622f6e
Alexander Adhyatma <alex@asiatech.dev>
2020-04-22 10:27:45 +0000

    adds authn initial commit

--- mod-account/server/authz/pkg/filters/jwt.go
+++ mod-account/server/authz/pkg/filters/jwt.go
@@ -5,8 +5,8 @@ import (
 	envoy_core "github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
 	envoy_auth "github.com/envoyproxy/go-control-plane/envoy/service/auth/v2"
 	envoy_type "github.com/envoyproxy/go-control-plane/envoy/type"
-	"github.com/getcouragenow/getcourage-packages/mod-account/authz/pkg/keyutil"
 	"github.com/getcouragenow/getcourage-packages/mod-account/authz/pkg/metadata"
+	keyutils "github.com/getcouragenow/getcourage-packages/mod-account/server/keyutils"
 	"github.com/gogo/googleapis/google/rpc"
 	"github.com/square/go-jose/v3/jwt"
 	rpcstatus "google.golang.org/genproto/googleapis/rpc/status"
@@ -14,7 +14,7 @@ import (
 	"time"
 )
 
-func tokenValidatorFunc(claim *keyutil.AppClaims) (bool, error) {
+func tokenValidatorFunc(claim *keyutils.AppClaims) (bool, error) {
 	//  TODO: validates more than this, i.e. audience & RBAC stuff
 	if claim.Issuer != os.Getenv("DEV_ISSUER") {
 		return false, errors.New("issuer is not valid")
@@ -26,7 +26,15 @@ func tokenValidatorFunc(claim *keyutil.AppClaims) (bool, error) {
 }
 
 func FilterJWT(token string) (bool, error) {
-	return keyutil.VerifyJwt(token, tokenValidatorFunc)
+	return keyutils.VerifyJwt(token, tokenValidatorFunc)
+}
+
+func GetClaim(token string) (*string, error) {
+	cl, err := keyutils.GetClaimsFromToken(token)
+	if err != nil {
+		return nil, err
+	}
+	return &cl.UserID, nil
 }
 
 // TODO: use denied response, as this is currently unused.
@@ -66,16 +74,21 @@ func GenGuestUser() []*envoy_core.HeaderValueOption {
 	return []*envoy_core.HeaderValueOption{
 		{
 			Header: &envoy_core.HeaderValue{
-				Key: metadata.HEADER_CLIENT_NAME,
+				Key:   metadata.HEADER_CLIENT_NAME,
 				Value: "guest",
 			},
 		},
 		{
 			Header: &envoy_core.HeaderValue{
-				Key: metadata.HEADER_IS_LOGGED_IN,
+				Key:   metadata.HEADER_USER_ID,
+				Value: "",
+			},
+		},
+		{
+			Header: &envoy_core.HeaderValue{
+				Key:   metadata.HEADER_IS_LOGGED_IN,
 				Value: "0",
 			},
 		},
 	}
 }
-
