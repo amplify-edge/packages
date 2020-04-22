6f8fd6d8fbedc0c1033630171afc9105569e51dd
winwisely199 <winwisely199@gmail.com>
2020-04-21 19:36:01 -0700

    #420 Adding delegate to example so it can run locally

--- mod-main/client/lib/modules/orgs/views/org_detail_view.dart
+++ mod-main/client/lib/modules/orgs/views/org_detail_view.dart
@@ -16,7 +16,7 @@ class OrgDetailView extends StatelessWidget {
         //   CarouselWithIndicator(imgList: campaign.videoURL),
         ListTile(
           title: Text(
-            ModMainLocalizations.of(context)?.translate('category') ?? 'Category',
+            ModMainLocalizations.of(context).translate('category'),
             style: Theme.of(context).textTheme.title,
           ),
           subtitle: Text(org.category),
