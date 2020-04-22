1bb1827eaf8b7cafbfc40a8c1de12e67b9c4705a
Alex Frank Adhyatma <alex@asiatech.dev>
2020-04-22 23:46:12 +0700

    added metadata header definition to keyutils

--- mod-account/server/keyutils/claims.go
+++ mod-account/server/keyutils/claims.go
@@ -20,6 +20,14 @@ type (
 	}
 )
 
+const (
+	HEADER_CLIENT_NAME = "x-client-role"
+	HEADER_USER_ID = "x-user-id"
+	HEADER_IS_LOGGED_IN = "x-client-is-logged-in"
+	ORIGINAL_TOKEN = "x-jwt-token"
+)
+
+
 func (k *KeyType) ReadSignerKey() (jose.Signer, error) {
 	return k.readPrivateSigner()
 }
