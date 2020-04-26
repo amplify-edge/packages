09943e34dfd0a0113c03d2780bd8aa7becd600ad
WinWisely268 <winwisely268@gmail.com>
2020-04-27 01:44:42 +0700

    all services deployed

--- maintemplate/server/cmd/appsecret/appsecret.go
+++ maintemplate/server/cmd/appsecret/appsecret.go
@@ -2,6 +2,7 @@ package main
 
 // Generate k8s secrets in secrets directory containing minio
 import (
+	"encoding/base64"
 	"flag"
 	"fmt"
 	"log"
@@ -45,14 +46,18 @@ func newAppSecrets(isMinio bool, name string) *appsecrets {
 	return &appsecrets{
 		MinioSecrets:   isMinio,
 		SecretsName:    name,
-		MinioAccessKey: os.Getenv("MINIO_ACCESSKEY"),
-		MinioSecretKey: os.Getenv("MINIO_SECRETKEY"),
-		MinioEncKey:    os.Getenv("MINIO_ENCKEY"),
-		MinioEndpoint:  os.Getenv("MINIO_ENDPOINT"),
-		MinioLocation:  os.Getenv("MINIO_LOCATION"),
-		MinioUseSSL:       os.Getenv("MINIO_SSL"),
-		MinioTimeout:   os.Getenv("MINIO_TIMEOUT"),
+		MinioAccessKey: toB64(os.Getenv("MINIO_ACCESSKEY")),
+		MinioSecretKey: toB64(os.Getenv("MINIO_SECRETKEY")),
+		MinioEncKey:    toB64(os.Getenv("MINIO_ENCKEY")),
+		MinioEndpoint:  toB64(os.Getenv("MINIO_ENDPOINT")),
+		MinioLocation:  toB64(os.Getenv("MINIO_LOCATION")),
+		MinioUseSSL:    toB64(os.Getenv("MINIO_SSL")),
+		MinioTimeout:   toB64(os.Getenv("MINIO_TIMEOUT")),
 	}
+}
+
+func toB64(inputstring string) string {
+	return base64.StdEncoding.EncodeToString([]byte(inputstring))
 }
 
 func main() {
--- maintemplate/server/scripts/source-env.sh
+++ maintemplate/server/scripts/source-env.sh
@@ -4,8 +4,11 @@ tail -n 20 ./ENV >> $HOME/.profile
 
 SHELLNAME='${SHELL##*/}'
 case $SHELLNAME in
-    "fish")
+    fish)
         bax source $HOME/.profile
+        ;;
+    zsh)
+        emulate sh -c '. $HOME/.profile'
         ;;
     *)
         source $HOME/.profile
