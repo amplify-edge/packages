103d27c57d1931945a55fba42a168df0792571c4
cloudwebrtc <duanweiwei1982@gmail.com>
2020-09-26 14:17:36 +0800

    Separate the accounts unit test from sys-core to dao.

--- sys-account/server/dao/dao_test.go
+++ sys-account/server/dao/dao_test.go
@@ -1,4 +1,4 @@
-package test_db
+package dao_test
 
 import (
 	"fmt"
@@ -6,17 +6,13 @@ import (
 	"testing"
 
 	"github.com/genjidb/genji"
-	"github.com/getcouragenow/packages/sys-accounts/accounts"
+	dao "github.com/getcouragenow/packages/sys-account/dao"
 	"github.com/getcouragenow/packages/sys-core/server/pkg/db"
 )
 
 var (
-	testDb *genji.DB
+	testDb *genji.DB = db.SharedDatabase()
 )
-
-func init() {
-	testDb = db.SharedDatabase()
-}
 
 func TestMakeSchema(t *testing.T) {
 	log.Print("MakeSchema testing .....")
@@ -29,27 +25,27 @@ func TestMakeSchema(t *testing.T) {
 func TestInsert(t *testing.T) {
 	log.Print("Insert testing .....")
 
-	o := accounts.Org{ID: db.UID(), Name: "org001"}
+	o := dao.Org{ID: db.UID(), Name: "org001"}
 	if err := o.Insert(testDb); err != nil {
 		t.Error(err)
 	}
 
-	u := accounts.User{ID: db.UID(), Name: "user1", Email: "example001@gmail.com"}
+	u := dao.User{ID: db.UID(), Name: "user1", Email: "example001@gmail.com"}
 	if err := u.Insert(testDb); err != nil {
 		t.Error(err)
 	}
 
-	p := accounts.Project{ID: db.UID(), Name: "proj001", OrgID: o.ID}
+	p := dao.Project{ID: db.UID(), Name: "proj001", OrgID: o.ID}
 	if err := p.Insert(testDb); err != nil {
 		t.Error(err)
 	}
 
-	r := accounts.Roles{ID: db.UID(), Role: "admin"}
+	r := dao.Roles{ID: db.UID(), Role: "admin"}
 	if err := r.Insert(testDb); err != nil {
 		t.Error(err)
 	}
 
-	pr := accounts.Permission{ID: db.UID(), User: u.Name, Org: o.Name, Project: p.Name, Role: r.Role}
+	pr := dao.Permission{ID: db.UID(), User: u.Name, Org: o.Name, Project: p.Name, Role: r.Role}
 	if err := pr.Insert(testDb); err != nil {
 		t.Error(err)
 	}
@@ -79,42 +75,42 @@ func TestInsert(t *testing.T) {
 }
 
 func TestQuery(t *testing.T) {
-	var o accounts.Org
+	var o dao.Org
 	sql := fmt.Sprintf("SELECT * FROM " + o.TableName() + " WHERE name = 'org002';")
 	if err := db.QueryTable(testDb, &o, sql, func(out interface{}) {
-		log.Printf("org => %v", out.(*accounts.Org))
+		log.Printf("org => %v", out.(*dao.Org))
 	}); err != nil {
 		t.Error(err)
 	}
 
-	var p accounts.Project
+	var p dao.Project
 	sql = fmt.Sprintf("SELECT * FROM " + p.TableName() + " WHERE name = 'proj002';")
 	if err := db.QueryTable(testDb, &p, sql, func(out interface{}) {
-		log.Printf("proj => %v", out.(*accounts.Project))
+		log.Printf("proj => %v", out.(*dao.Project))
 	}); err != nil {
 		t.Error(err)
 	}
 
-	var u accounts.User
+	var u dao.User
 	sql = fmt.Sprintf("SELECT * FROM " + u.TableName() + " WHERE name = 'user2';")
 	if err := db.QueryTable(testDb, &u, sql, func(out interface{}) {
-		log.Printf("user => %v", out.(*accounts.User))
+		log.Printf("user => %v", out.(*dao.User))
 	}); err != nil {
 		t.Error(err)
 	}
 
-	var r accounts.Roles
+	var r dao.Roles
 	sql = fmt.Sprintf("SELECT * FROM " + r.TableName() + " WHERE role = 'user';")
 	if err := db.QueryTable(testDb, &r, sql, func(out interface{}) {
-		log.Printf("role => %v", out.(*accounts.Roles))
+		log.Printf("role => %v", out.(*dao.Roles))
 	}); err != nil {
 		t.Error(err)
 	}
 
-	var pr accounts.Permission
+	var pr dao.Permission
 	sql = fmt.Sprintf("SELECT * FROM " + pr.TableName() + " WHERE org = 'org002' AND user = 'user2';")
 	if err := db.QueryTable(testDb, &pr, sql, func(out interface{}) {
-		log.Printf("promission => %v", out.(*accounts.Permission))
+		log.Printf("promission => %v", out.(*dao.Permission))
 	}); err != nil {
 		t.Error(err)
 	}
@@ -122,35 +118,35 @@ func TestQuery(t *testing.T) {
 
 func TestDelete(t *testing.T) {
 	log.Print("Clanup all tables .....")
-	o := accounts.Org{}
+	o := dao.Org{}
 	sql := "DELETE FROM " + o.TableName() + " WHERE name = 'org002';"
 	log.Printf("DELETE Table: %v\n sql = %v", o.TableName(), sql)
 	if err := testDb.Exec(sql); err != nil {
 		t.Error(err)
 	}
 
-	u := accounts.User{}
+	u := dao.User{}
 	sql = "DELETE FROM " + u.TableName() + " WHERE name = 'user2';"
 	log.Printf("DELETE Table: %v\n sql = %v", u.TableName(), sql)
 	if err := testDb.Exec(sql); err != nil {
 		t.Error(err)
 	}
 
-	p := accounts.Project{}
+	p := dao.Project{}
 	sql = "DELETE FROM " + p.TableName() + " WHERE name = 'proj002';"
 	log.Printf("DELETE Table: %v\n sql = %v", p.TableName(), sql)
 	if err := testDb.Exec(sql); err != nil {
 		t.Error(err)
 	}
 
-	r := accounts.Roles{}
+	r := dao.Roles{}
 	sql = "DELETE FROM " + r.TableName() + " WHERE role = 'user';"
 	log.Printf("DELETE Table: %v\n sql = %v", r.TableName(), sql)
 	if err := testDb.Exec(sql); err != nil {
 		t.Error(err)
 	}
 
-	pr := accounts.Permission{}
+	pr := dao.Permission{}
 	sql = "DELETE FROM " + pr.TableName() + " WHERE org = 'org002' AND user = 'user2';"
 	log.Printf("DELETE Table: %v\n sql = %v", pr.TableName(), sql)
 	if err := testDb.Exec(sql); err != nil {
@@ -165,42 +161,42 @@ func TestFinalResult(t *testing.T) {
 }
 
 func printTables(t *testing.T) {
-	var o accounts.Org
+	var o dao.Org
 	sql := fmt.Sprintf("SELECT * FROM " + o.TableName() + ";")
 	if err := db.QueryTable(testDb, &o, sql, func(out interface{}) {
-		log.Printf("org => %v", out.(*accounts.Org))
+		log.Printf("org => %v", out.(*dao.Org))
 	}); err != nil {
 		t.Error(err)
 	}
 
-	var p accounts.Project
+	var p dao.Project
 	sql = fmt.Sprintf("SELECT * FROM " + p.TableName() + ";")
 	if err := db.QueryTable(testDb, &p, sql, func(out interface{}) {
-		log.Printf("proj => %v", out.(*accounts.Project))
+		log.Printf("proj => %v", out.(*dao.Project))
 	}); err != nil {
 		t.Error(err)
 	}
 
-	var u accounts.User
+	var u dao.User
 	sql = fmt.Sprintf("SELECT * FROM " + u.TableName() + ";")
 	if err := db.QueryTable(testDb, &u, sql, func(out interface{}) {
-		log.Printf("user => %v", out.(*accounts.User))
+		log.Printf("user => %v", out.(*dao.User))
 	}); err != nil {
 		t.Error(err)
 	}
 
-	var r accounts.Roles
+	var r dao.Roles
 	sql = fmt.Sprintf("SELECT * FROM " + r.TableName() + ";")
 	if err := db.QueryTable(testDb, &r, sql, func(out interface{}) {
-		log.Printf("role => %v", out.(*accounts.Roles))
+		log.Printf("role => %v", out.(*dao.Roles))
 	}); err != nil {
 		t.Error(err)
 	}
 
-	var pr accounts.Permission
+	var pr dao.Permission
 	sql = fmt.Sprintf("SELECT * FROM " + pr.TableName() + ";")
 	if err := db.QueryTable(testDb, &pr, sql, func(out interface{}) {
-		log.Printf("promission => %v", out.(*accounts.Permission))
+		log.Printf("promission => %v", out.(*dao.Permission))
 	}); err != nil {
 		t.Error(err)
 	}
--- sys-core/server/go.mod
+++ sys-core/server/go.mod
@@ -2,12 +2,9 @@ module github.com/getcouragenow/packages/sys-core/server
 
 go 1.14
 
-replace github.com/getcouragenow/packages/sys-accounts => ../../sys-account/server
-
 require (
 	github.com/dgraph-io/badger/v2 v2.2007.2
 	github.com/genjidb/genji v0.7.1
 	github.com/genjidb/genji/engine/badgerengine v0.7.1
-	github.com/getcouragenow/packages/sys-accounts v0.0.0-00010101000000-000000000000
 	github.com/segmentio/ksuid v1.0.3
 )
